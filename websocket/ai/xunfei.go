37889497911ced3197dc6cf381f792c64e322542
whitexiong <986247535@qq.com>
2023-11-23 13:17:23 +0800

    添加v3、微调版本

--- websocket/ai/xunfei.go
+++ websocket/ai/xunfei.go
@@ -21,7 +21,7 @@ import (
 func AssembleAuthUrl() string {
 	apiSecret := os.Getenv("AI_API_SECRET")
 	apiKey := os.Getenv("AI_API_KEY")
-	hostUrl := os.Getenv("AI_HOST_URL")
+	hostUrl := os.Getenv("AI_HOST_URL_v3")
 	ul, err := url.Parse(hostUrl)
 	if err != nil {
 		fmt.Println(err)
@@ -56,23 +56,50 @@ func GenParams(chatRoomID uint, question string) map[string]interface{} {
 		"content": question,
 	}
 	chatHistory = append(chatHistory, currentMessage)
-	log.Printf("上下文：%d\n", chatHistory)
+	log.Printf("上下文：%v\n", chatHistory)
+
+	// 添加 Function Call 后期插件调用
+	//functionCall := map[string]interface{}{
+	//	"text": []map[string]interface{}{
+	//		{
+	//			"name":        "天气查询",
+	//			"description": "天气插件可以提供天气相关信息。你可以提供指定的地点信息、指定的时间点或者时间段信息，来检索诗词库，精准检索到天气信息。",
+	//			"parameters": map[string]interface{}{
+	//				"type": "object",
+	//				"properties": map[string]interface{}{
+	//					"location": map[string]interface{}{
+	//						"type":        "string",
+	//						"description": "地点，比如北京。",
+	//					},
+	//					"date": map[string]interface{}{
+	//						"type":        "string",
+	//						"description": "日期。",
+	//					},
+	//				},
+	//				"required": []string{"location"},
+	//			},
+	//		},
+	//		// 这里可以继续添加其他 Function
+	//	},
+	//}
 
 	return map[string]interface{}{
 		"header": map[string]interface{}{
 			"app_id": os.Getenv("AI_APP_ID"),
+			//"patch_id": []string{""}, //微调的模型资源ID
 		},
 		"parameter": map[string]interface{}{
 			"chat": map[string]interface{}{
-				"domain":      "generalv2", // 注意：示例中是"general"而不是"generalv2"
-				"temperature": 0.5,         // 根据示例，值调整为0.5
-				"max_tokens":  1024,        // 根据示例，值调整为1024
+				"domain":      "generalv3",
+				"temperature": 0.5,
+				"max_tokens":  1024,
 			},
 		},
 		"payload": map[string]interface{}{
 			"message": map[string]interface{}{
-				"text": chatHistory, // 整个聊天历史，包括最新的消息
+				"text": chatHistory,
 			},
+			//"functions": functionCall, // 添加到请求中
 		},
 	}
 }
@@ -120,6 +147,9 @@ func StartChatWithAI(chatRoomID uint, question string) string {
 			break
 		}
 
+		jsonString := string(msg)
+		fmt.Println("流式数据:", jsonString)
+
 		var data map[string]interface{}
 		if err := json.Unmarshal(msg, &data); err != nil {
 			fmt.Println("解析JSON错误:", err)
@@ -129,6 +159,7 @@ func StartChatWithAI(chatRoomID uint, question string) string {
 		payload, exists := data["payload"].(map[string]interface{})
 		if !exists {
 			fmt.Println("解析payload错误")
+			fmt.Printf("完整响应数据: %+v\n", data)
 			return answer
 		}
 
@@ -163,6 +194,34 @@ func StartChatWithAI(chatRoomID uint, question string) string {
 			return answer
 		}
 
+		//for _, t := range text {
+		//	textMap, ok := t.(map[string]interface{})
+		//	if !ok {
+		//		continue
+		//	}
+		//
+		//	// 检查并处理 function_call
+		//	if functionCall, exists := textMap["function_call"]; exists {
+		//		fmt.Println("Function Call 响应:", functionCall)
+		//		// 根据 function_call 的内容执行相应操作
+		//		if functionName, ok := functionCall.(map[string]interface{})["name"].(string); ok {
+		//			switch functionName {
+		//			case "天气查询":
+		//				// 解析参数并执行天气查询
+		//				arguments := functionCall.(map[string]interface{})["arguments"].(string)
+		//				// 这里调用一个函数来处理天气查询，例如：
+		//				weatherInfo := queryWeather(arguments)
+		//				answer += weatherInfo
+		//			}
+		//		}
+		//	}
+		//
+		//	// 累加其他文本内容
+		//	if content, exists := textMap["content"].(string); exists && content != "" {
+		//		answer += content
+		//	}
+		//}
+
 		answer += content
 		if status == 2 {
 			conn.Close()
@@ -172,6 +231,10 @@ func StartChatWithAI(chatRoomID uint, question string) string {
 
 	fmt.Println("AI回复:", answer)
 	return answer
+}
+
+func queryWeather(arguments string) string {
+	return "测试 func"
 }
 
 func HmacWithShaTobase64(signString []string, key string) string {
@@ -202,7 +265,8 @@ func GetChatHistoryForRoom(chatRoomID uint) []map[string]interface{} {
 	models.DB.Where("chat_room_id = ?", chatRoomID).Order("created_at desc").Limit(3).Find(&messages)
 
 	var chatHistory []map[string]interface{}
-	for _, msg := range messages {
+	for i := len(messages) - 1; i >= 0; i-- { // 反向遍历消息
+		msg := messages[i]
 		role := "user"
 		if msg.AIProvider != models.None {
 			role = "assistant"
